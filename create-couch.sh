63aa705a8ce17a00192d419356452990839996c7
Nic Zemke <nic@transparency-everywhere.com>
2018-12-21 16:14:58 +0100

    create-couch.sh updated

--- create-couch.sh
+++ create-couch.sh
@@ -1,1 +1,16 @@
-curl -X PUT 'http:/127.0.0.1:5984/items/VEHICLE_SW3' -d '{"template":"vehicle", "propterties": {"name": "Sea-Watch 3"}}'
+
+echo "Please enter the URL of your  couchDB instance and press [ENTER]:"
+
+read db_url
+
+#db_url="http://159.89.111.136:5984"
+
+#Create Items DB
+curl -X PUT "${db_url}/items"
+
+#Create Positions DB
+curl -X PUT "${db_url}/positions"
+
+
+#Create SW3 Item
+curl -X PUT "${db_url}/items/VEHICLE_SW3" -d '{"_id": "VEHICLE_SW3","key": "VEHICLE_SW3","doc": {"identifier": "SW3","template": "vehicle","properties": { "name": "Sea-Watch 3","tracking_type": "AIS","MMSI": "244140096"}}}'
