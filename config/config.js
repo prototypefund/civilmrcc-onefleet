3adb062eafb977bc95ed9cf195c4566fd51c3b7b
niczem <nic@sea-watch.org>
2019-03-12 13:01:48 +0100

    module.exports changed to export default

--- config/config.js
+++ config/config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const config = {
     db_remote_host:'localhost',
     db_remote_port:5984
-}
+};
+
+export default {
+    db_remote_host:'localhost',
+    db_remote_port:5984
+};
