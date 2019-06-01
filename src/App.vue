c3c3e179d75710eb03f04056137186d9ec2d09ef
niczem <nic@sea-watch.org>
2019-06-01 16:20:05 +0200

    select style changed

--- src/App.vue
+++ src/App.vue
@@ -227,4 +227,39 @@ ul{
 .form-style-6 input[type="button"]:hover{
   background: #2EBC99;
 }
+.select-css {
+  display: block;
+  font-size: 16px;
+  font-family: sans-serif;
+  font-weight: 700;
+  color: #444;
+  line-height: 1.3;
+  padding: .6em 1.4em .5em .8em;
+  width: 100%;
+  max-width: 100%; 
+  box-sizing: border-box;
+  margin: 0;
+  border: 1px solid #000;
+  border-radius: 0;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #fff;
+}
+.select-css::-ms-expand {
+  display: none;
+}
+.select-css:hover {
+  border-color: #888;
+}
+.select-css:focus {
+  border-color: #aaa;
+  box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
+  box-shadow: 0 0 0 3px -moz-mac-focusring;
+  color: #222; 
+  outline: none;
+}
+.select-css option {
+  font-weight:normal;
+}
 </style>
