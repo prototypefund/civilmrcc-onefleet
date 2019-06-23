898468434484a7c0273eb933cc60d3a52a662bc7
niczem <nic@sea-watch.org>
2019-06-23 03:19:04 +0200

    bootstrap vue removed

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import App from './App.vue'
 
 import dbWrapper from './utils/dbWrapper'
