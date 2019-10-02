2b44dbd8cd6b2b24374bc425bc5347321b9f637e
niczem <nic@sea-watch.org>
2019-10-02 19:01:26 +0200

    dbwrapper moved to npm module

--- src/utils/dbWrapper.js
+++ src/utils/dbWrapper.js
@@ -1,108 +1,13 @@
-import PouchDB from 'pouchdb-browser'
-import PouchDBAuthentication from 'pouchdb-authentication'
-import config from '../../config/config.js';
+import pouchwrapper from 'pouchwrapper'
 import { serverBus } from '../main';
-PouchDB.plugin(PouchDBAuthentication);
+import config from '../../config/config.js';
 
-var dbWrapper = function(){
-    this.databases = {};
-    this.logged_in;
+console.log('CONFIG');
+console.log(config);
 
-    this.initDB = function(db_name){
-      var self = this;
-      if(typeof this.databases[db_name] == 'undefined'){
-            this.databases[db_name] = {
-              local: new PouchDB(db_name, {skip_setup:false}),
-              remote: new PouchDB(this.getDBURL()+config.db_prefix+db_name+'?include_docs=true&descending=true', {skip_setup:false}),   
-            }
+pouchwrapper.setConfig(config);
 
-            this.databases[db_name].local.replicate.from(this.databases[db_name].remote).on('complete', function (r,a,n) {
-            // yay, we're done!
-            console.log('initial replication done!');
-            console.log('starting sync..');
-
-            self.databases[db_name].remote.sync(db_name, {
-              live: true,
-              retry: true
-            }).on('change', function (change) {
-              console.log('data ch change', change);
-              //each database can contain multiple onchange listeners, defined by index n
-              for(let n in self.databases[db_name].onChange){
-                if(typeof(self.databases[db_name].onChange[n]) == 'function'){
-                  self.databases[db_name].onChange[n]();
-                }
-              }
-            }).on('error', function (err) {
-              console.log('sync error', err);
-              if(err.error === 'unauthorized'){
-                localStorage.removeItem(username);
-                localStorage.removeItem(password);
-                window.location.reload();
-              }
-            });
-
-          }).on('error', function (err) {
-
-            console.log('error during inital replication:');
-            console.log(err);
-            if(err.error === 'unauthorized'){
-                localStorage.clear();
-                window.location.reload();
-            }
-            // boo, something went wrong!
-          });
-
-      }
-    }
-   /**
-   * Sets onchange funtions for a specified database
-   *
-   * @param {string} db_name - Database selector
-   * @param {string} method_name - Index of the method
-   * @param {functoin} method - Function that is executed on db-change
-   */
-    this.setOnChange = function(db_name,method_name,method){
-        var db = this.getDB(db_name);
-        if(typeof this.databases[db_name].onChange == 'undefined'){
-          this.databases[db_name].onChange = {}
-        }
-        this.databases[db_name].onChange[method_name] = method;
-    }
-    this.getDB = function(db_name){
-        
-        if(typeof this.databases[db_name] == 'undefined'){
-            this.initDB(db_name)
-        }
-        return this.databases[db_name].local;
-    };
-    this.showLogin = function(){
-        serverBus.$emit('modal_modus', 'login');
-        //this.login('sw3','password',this.getDB('items'))
-    };
-    this.login = function(username, password, db){
-        db.login(username, password).then(function(res) {
-          console.log(res);
-          localStorage.username = username;
-          localStorage.password = password;
-        }).then(function(docs) {
-
-        }).catch(function(error) {
-          console.error(error);
-        });
-    }
-    this.fetchError = function(err){
-        console.log(err);
-        if(err.error == "unauthorized")
-            this.showLogin();
-    }
-    this.getDBURL = function(){
-        if(typeof localStorage.username != 'undefined' && localStorage.username.length > 0)
-
-            return 'https://'+localStorage.username+':'+localStorage.password+'@'+config.db_remote_host+':'+config.db_remote_port+'/';
-        else    
-            this.showLogin();
-    }
-    this.getPositionsForItem = function(identifier,cb){
+pouchwrapper.getPositionsForItem = function(identifier,cb){
         var self = this;
         let result  = [];
         this.getDB('positions').allDocs({
@@ -130,8 +35,8 @@ var dbWrapper = function(){
           self.fetchError(err);
         });
 
-    }
-    this.createPosition = function(obj,cb){
+}
+pouchwrapper.createPosition = function(obj,cb){
       this.getDB('positions').put(obj).then(function (response) {
         console.log('position created');
         console.log(response)
@@ -140,7 +45,7 @@ var dbWrapper = function(){
         cb(err);
       });
     };
-    this.createItem = function(obj,cb){
+pouchwrapper.createItem = function(obj,cb){
       var itemDB = this.getDB('items');
       itemDB.put(obj).then(function (response) {
         console.log('item created');
@@ -150,12 +55,12 @@ var dbWrapper = function(){
         cb(err);
       });
     };
-    this.updateItem = function(obj,cb){
+pouchwrapper.updateItem = function(obj,cb){
       //in pouch create and update actually is the same function
       //an item will be updated if the id and a rev already exists
       return this.createItem(obj,cb);
     }
-    this.getItem = function(itemId,cb){
+pouchwrapper.getItem = function(itemId,cb){
       var self = this;
       this.getDB('items').get(itemId).then(function (doc) {
         self.getPositionsForItem(doc.identifier,function(positions){
@@ -170,7 +75,7 @@ var dbWrapper = function(){
         console.log(err);
       });
     }
-    this.getItems = function(cb){
+pouchwrapper.getItems = function(cb){
         var items = this.getDB('items');
         var self = this;
         items.allDocs({
@@ -198,7 +103,7 @@ var dbWrapper = function(){
           cb(err);
         });
     }
-    this.getItemsByTemplate = function(template,cb){
+pouchwrapper.getItemsByTemplate = function(template,cb){
         var self = this;
 
         console.log('getItemsByTemplate');
@@ -238,10 +143,10 @@ var dbWrapper = function(){
           cb(err)
         });
     }
-    this.getVehicles = function(cb){
+pouchwrapper.getVehicles = function(cb){
         this.getItemsByTemplate('VEHICLE',cb);
     }
-    this.appendItemsToMap = function(map, options){
+pouchwrapper.appendItemsToMap = function(map, options){
       this.getItems(function(err,result){
         for(var i in result.rows){
           var item = result.rows[i];
@@ -252,7 +157,7 @@ var dbWrapper = function(){
       });
     }
 
-    this.updateShownItemsOnMap = function(map,options){
+pouchwrapper.updateShownItemsOnMap = function(map,options){
         //item is here not the same as an item in the database
         //it could be a L.marker, L.polygon etc
         
@@ -270,5 +175,4 @@ var dbWrapper = function(){
 
         
     }
-}
-export default new dbWrapper();
+export default pouchwrapper;
