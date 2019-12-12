d7dbfa36573b0ebd01f1778a5f3df30a957910c6
anke weber <anke.weber@centigrade.de>
2019-12-12 19:15:56 +0100

    fix: cases label in dropdown list is clickable

--- src/components/LeftNavigation.vue
+++ src/components/LeftNavigation.vue
@@ -6,9 +6,19 @@
         <el-collapse-item v-for="category in categories" class="categories" :title="category.plural" :name="category.plural" :key="category.plural">
           <ul class="category_list">
             <li class="list_item" v-for="(item, index) in category.items.rows" :key="index">
-              <span class="item_name" @click="clickItem(item.id)" v-if="item.doc.properties.name">{{item.doc.properties.name}}</span>
-              <span class="item_name" v-if="!item.doc.properties.name">{{item.doc._id}}</span>
-              <span class="list_item_tag"><el-tag size="small" :type="getTimeTagType(item)" style="width:100px" v-if="item.positions&&item.positions.length>0&&item.positions[item.positions.length-1]">{{showTimeTag(item)}} ago</el-tag><el-tag size="small" type="info" style="width:100px" v-if="!item.positions||item.positions.length==0">no positions</el-tag></span>
+
+
+              <span class="item_name" @click="clickItem(item.id)" v-if="item.doc.properties.name">
+                {{item.doc.properties.name}}</span>
+              <span class="item_name" @click="clickItem(item.id)" v-if="!item.doc.properties.name">{{item.doc._id}}</span>
+
+
+              <span class="list_item_tag"><el-tag size="small" :type="getTimeTagType(item)" style="width:100px" 
+              v-if="item.positions&&item.positions.length>0&&item.positions[item.positions.length-1]">
+              {{showTimeTag(item)}} ago</el-tag><el-tag size="small" type="info" style="width:100px" 
+              v-if="!item.positions||item.positions.length==0">no positions</el-tag></span>
+
+
               <span class="list_item_switch">
                 <el-switch
                 v-model="shown_items[item.id]"
@@ -228,7 +238,6 @@ el-switch{
 }
 
 .item_name {
-  background-color: red;
   word-wrap: break-word;
 }
 
