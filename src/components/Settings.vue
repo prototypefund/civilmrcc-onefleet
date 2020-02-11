ca2dd3293c07862fded7d7d218ab3bc1b74b64af
niczem <nic@sea-watch.org>
2020-02-11 20:00:17 +0100

    toggle for positiontimestamps are added to settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -43,6 +43,9 @@
         <br />
         <span>Always show item captions</span>
         <input type="checkbox" value="true" v-model="settings.showcaptions" />
+        <br />
+        <span>Show position timestamps</span>
+        <input type="checkbox" value="true" v-model="settings.positiontimestamps" />
 
         <input type="submit" value="Save and reload" />
       </form>
@@ -61,6 +64,7 @@ export default {
         maptiles: localStorage.settings_maptiles || 'openlayers',
         openseamap: localStorage.settings_openseamap == 'true' || false,
         showcaptions: localStorage.settings_showcaptions == 'true' || false,
+        positiontimestamps: localStorage.settings_positiontimestamps == 'true' || false,
         max_track_length: localStorage.settings_map_track_length || 100,
         max_track_type: localStorage.settings_max_track_type || 'number_of_positions',
         track_startdate: localStorage.settings_track_startdate || new Date().toLocaleString('en-US'),
@@ -78,6 +82,7 @@ export default {
       localStorage.settings_maptiles = this.settings.maptiles;
       localStorage.settings_openseamap = this.settings.openseamap;
       localStorage.settings_showcaptions = this.settings.showcaptions;
+      localStorage.settings_positiontimestamps = this.settings.positiontimestamps;
 
       localStorage.settings_max_track_type = this.settings.max_track_type;
       if (this.settings.max_track_type == 'date_range') {
